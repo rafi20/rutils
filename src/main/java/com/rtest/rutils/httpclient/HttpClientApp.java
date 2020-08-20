fb969b3aa1b871b3952cc0680cd064369a85c2bb
Mohammed Rafi <mohammed.rafi@e2open.com>
2020-08-20 18:55:01 +0530

    ssl http client

---
+++ src/main/java/com/rtest/rutils/httpclient/HttpClientApp.java
@@ -0,0 +1,51 @@
+package com.rtest.rutils.httpclient;
+
+import static java.net.http.HttpRequest.newBuilder;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.logging.Logger;
+public final class HttpClientApp {
+    private static Logger logger = Logger.getLogger(HttpClientApp.class.getName());
+    private static HttpClient client = HttpClient.newHttpClient();
+    public static void main(String[] args) throws Exception {
+        String url = "http://localhost:8080/nifi-api/flow/about";
+        String dev_url1 = "https://iacap063.dev.e2open.com:9003/nifi-api/flow/about";
+		String dev_url = "https://iacap063.dev.e2open.com:9003/nifi-api/system-diagnostics";
+        String postname_url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2";
+        httpGetRequest(dev_url);
+    }
+    static void httpGetRequest(String url) throws URISyntaxException, IOException, InterruptedException {
+        URI httpURI = new URI(url);
+        HttpRequest request = newBuilder(httpURI).GET()
+                .headers("Accept-Enconding", "gzip, deflate").build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String responseBody = response.body();
+        int statusCode = response.statusCode();
+        System.out.println(responseBody + ", " + statusCode);
+    }
+}
+
+/*
+#Client usage examples:
+# TLSv1.2
+java -Djdk.tls.client.protocols=TLSv1.2 -Djavax.net.debug=all -Djavax.net.ssl.keyStore=/nas_k8s_data/rdm-qa/conf/clientcerts/externalKeystore.jks -Djavax.net.ssl.keyStorePassword=e2RdmNifi@P -Djavax.net.ssl.trustStore=/nas_k8s_data/rdm-qa/conf/clientcerts/externalTruststore.jks -Djavax.net.ssl.trustStorePassword=e2RdmNifi@P HttpClientApp
+
+# TLSv1.2, use p12 file instead of jks
+java -Djdk.tls.client.protocols=TLSv1.2 -Djavax.net.debug=all -Djavax.net.ssl.keyStore=/nas_k8s_data/rdm-qa/conf/clientcerts/cdmqa-rdmnifi-admin.p12 -Djavax.net.ssl.keyStorePassword=e2RdmNifi@P -Djavax.net.ssl.trustStore=/nas_k8s_data/rdm-qa/conf/clientcerts/externalTruststore.jks -Djavax.net.ssl.trustStorePassword=e2RdmNifi@P HttpClientApp
+
+#TLSv1.3 (default)
+java -Djavax.net.debug=all -Djavax.net.ssl.keyStore=/nas_k8s_data/rdm-qa/conf/clientcerts/externalKeystore.jks -Djavax.net.ssl.keyStorePassword=e2RdmNifi@P -Djavax.net.ssl.trustStore=/nas_k8s_data/rdm-qa/conf/clientcerts/externalTruststore.jks -Djavax.net.ssl.trustStorePassword=e2RdmNifi@P HttpClientApp
+
+# TLSv1.3, use p12 file instead of jks
+java -Djavax.net.debug=all -Djavax.net.ssl.keyStore=/nas_k8s_data/rdm-qa/conf/clientcerts/nas_k8s_data/rdm-qa/conf/clientcertscert/cdmqa-rdmnifi-admin.p12 -Djavax.net.ssl.keyStorePassword=e2RdmNifi@P -Djavax.net.ssl.trustStore=/nas_k8s_data/rdm-qa/conf/clientcerts/externalTruststore.jks -Djavax.net.ssl.trustStorePassword=e2RdmNifi@P HttpClientApp
+
+#Error: without truststore with (ca.crt) client certificate wont be presented to server
+java -Djavax.net.debug=all -Djavax.net.ssl.keyStore=/nas_k8s_data/rdm-qa/conf/clientcerts/nas_k8s_data/rdm-qa/conf/clientcertscert/cdmqa-rdmnifi-admin.p12 -Djavax.net.ssl.keyStorePassword=e2RdmNifi@P -Djavax.net.ssl.trustStore=/nas_k8s_data/rdm-qa/conf/clientcerts/externalTruststore.jks -Djavax.net.ssl.trustStorePassword=e2RdmNifi@P HttpClientApp
+
+
+*/
