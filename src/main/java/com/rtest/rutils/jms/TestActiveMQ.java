e8d029314abb124ba7e1fba00543b0fa984d1305
Mohammed Rafi <mohammed.rafi@e2open.com>
2020-09-18 20:05:28 +0530

    added amq config

--- src/main/java/com/rtest/rutils/jms/TestActiveMQ.java
+++ src/main/java/com/rtest/rutils/jms/TestActiveMQ.java
@@ -12,3 +12,38 @@ public class TestActiveMQ {
 		}		
 	}
 }
+
+/*
+#Configure ActiveMQ authentication/Authorization
+#In activemq.xml
+<plugins>
+    <simpleAuthenticationPlugin anonymousAccessAllowed="false">
+        <users>
+            <authenticationUser username="minifi" password="minifi" groups="admins, producers, consumers"/>
+        </users>
+    </simpleAuthenticationPlugin>
+
+
+    <authorizationPlugin>
+        <map>
+            <authorizationMap>
+                <authorizationEntries>
+					<authorizationEntry queue="test_minifi" read="consumers" write="producers" admin="producers,consumers,admins" /> 
+                    <authorizationEntry queue="myQueue" read="consumers" write="producers" admin="producers,consumers,admins" />    
+                    <authorizationEntry topic="ActiveMQ.Advisory.>" read="consumers" write="producers" admin="producers,consumers,admins"/>
+                </authorizationEntries>
+            </authorizationMap>
+        </map>
+    </authorizationPlugin>
+</plugins>
+
+#Client
+https://repo1.maven.org/maven2/org/apache/activemq/activemq-all/5.15.9/activemq-all-5.15.9.jar
+https://repo1.maven.org/maven2/org/apache/activemq/activemq-client/5.15.9/activemq-client-5.15.9.jar
+
+#Admin 
+http://localhost:8161/admin/
+admin/admin
+admin/manager
+
+*/
